f9446ab1fd4ac9110f30ec828868b68fbcd0c3ec
Muhammad Azamuddin <azamuddin@live.com>
2015-12-29 17:52:06 +0700

    Adding webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+    entry: [
+//        'webpack-dev-server/client?http://localhost:8000',
+//        'webpack/hot/only-dev-server',
+        './index.js'
+    ],
+
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist',
+        publicPath: 'http://localhost:8000/static'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/ ,
+                exclude: /node_modules/,
+                loaders: ['react-hot','babel?presets[]=es2015,presets[]=react']
+            }
+        ]
+    }
+
+}
